36207e70a49299c84a2a310dde6bb3f97b8fc78e
Hans Markwart <h.markwart@mueller-wulff.de>
2025-11-17 10:02:55 +0100

    initial commit

---
+++ composeApp/src/iosMain/kotlin/eu/heha/conifer/MainViewController.kt
@@ -0,0 +1,5 @@
+package eu.heha.conifer
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/webMain/kotlin/eu/heha/conifer/main.kt
@@ -0,0 +1,11 @@
+package eu.heha.conifer
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
