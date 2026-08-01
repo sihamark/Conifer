6389898b7ac37aa5fb50918e2eb22552a049d302
Hans Markwart <h.markwart@mueller-wulff.de>
2026-08-01 15:45:47 +0200

    - updated proguard rules for desktop because they lead to the database not being constructed - updated proguard rules for android because they were obsessive

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -6,8 +6,8 @@ object AppConfig {
     const val appName = "Conifer"
     const val minSdk = 30
     const val targetSdk = 37
-    const val versionCode = 5
-    const val versionName = "1.2.0"
+    const val versionCode = 6
+    const val versionName = "1.2.1"
     const val javaVersion = 21
     const val namespace = "eu.heha.conifer"
 }
